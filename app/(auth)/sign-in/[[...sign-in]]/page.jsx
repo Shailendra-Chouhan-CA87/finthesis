40d73717b7d6559b1c8aecd8efc8731b081c52a2
shailendra3660 <chouhanshailendra74@gmail.com>
2025-03-20 16:23:51 +0530

    Design the database schema's and adding the model's (user, transaction, budget, account) into our supabase project

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,10 +1,6 @@
-import { SignIn } from '@clerk/nextjs';
-import React from 'react'
 
-const Page = () => {
-  return (
-    <SignIn />
-  )
-};
+import { SignIn } from "@clerk/nextjs";
 
-export default Page;
+export default function Page() {
+  return <SignIn />;
+}
